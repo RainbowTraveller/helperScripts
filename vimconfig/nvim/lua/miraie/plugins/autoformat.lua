3bcec947742b91c4c2634256e7f7b7dc5eec8063
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-08-02 19:23:18 -0700

    Correction in calling the autoformat plugin

--- vimconfig/nvim/lua/miraie/plugins/autoformat.lua
+++ vimconfig/nvim/lua/miraie/plugins/autoformat.lua
@@ -1,26 +1,44 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {
-		notify_on_error = false,
+	config = function()
+		local opts = {
 
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+				lsp_format = "fallback",
+			},
 
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
-			python = function(bufnr)
-				if require("conform").get_formatter_info("ruff_format", bufnr).available then
-					return { "ruff_format" }
-				else
-					return { "isort", "black" }
-				end
-			end,
-			go = { "gofmt" },
-			java = { "google-java-format" },
-			csharp = { "csharpier" },
-		},
-	},
+			format_after_save = {
+				lsp_format = "fallback",
+			},
+
+			-- Conform will notify you when a formatter errors
+			notify_on_error = true,
+			-- Conform will notify you when no formatters are available for the buffer
+			notify_no_formatters = true,
+
+			formatters_by_ft = {
+				-- Use the "*" filetype to run formatters on all filetypes.
+				lua = { "stylua" },
+				-- Conform can also run multiple formatters sequentially
+				python = function(bufnr)
+					if require("conform").get_formatter_info("ruff_format", bufnr).available then
+						return { "ruff_format" }
+					else
+						return { "isort", "black" }
+					end
+				end,
+				go = { "goimports", "gofmt" },
+				java = { "google-java-format" },
+				scala = { "scalafmt" },
+				-- csharp = { "csharpier" },
+				["*"] = { "codespell" },
+				-- Use the "_" filetype to run formatters on filetypes that don't
+				-- have other formatters configured.
+				["_"] = { "trim_whitespace" },
+			},
+		}
+		require("conform").setup(opts)
+	end,
 }
