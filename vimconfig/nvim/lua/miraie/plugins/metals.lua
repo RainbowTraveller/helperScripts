6e3d8adaf10a6d96d316e8b9afc4b3eaf7757fe6
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-08-07 23:47:49 -0700

    Moved mappings to either autocmd or remap

--- vimconfig/nvim/lua/miraie/plugins/metals.lua
+++ vimconfig/nvim/lua/miraie/plugins/metals.lua
@@ -1,4 +1,4 @@
--- https://github.com/scalameta/nvim-metals/discussions/39
+-- https:l/github.com/scalameta/nvim-metals/discussions/39
 return {
 	"scalameta/nvim-metals",
 	dependencies = {
@@ -61,77 +61,6 @@ return {
 
 		metals_config.on_attach = function(client, bufnr)
 			require("metals").setup_dap()
-
-			-- LSP mappings
-			vim.keymap.set("n", "gD", vim.lsp.buf.definition)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references)
-			vim.keymap.set("n", "gds", vim.lsp.buf.document_symbol)
-			vim.keymap.set("n", "gws", vim.lsp.buf.workspace_symbol)
-			vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run)
-			vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help)
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-			vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-
-			vim.keymap.set("n", "<leader>ws", function()
-				require("metals").hover_worksheet()
-			end)
-
-			-- all workspace diagnostics
-			vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist)
-
-			-- all workspace errors
-			vim.keymap.set("n", "<leader>ae", function()
-				vim.diagnostic.setqflist({ severity = "E" })
-			end)
-
-			-- all workspace warnings
-			vim.keymap.set("n", "<leader>aw", function()
-				vim.diagnostic.setqflist({ severity = "W" })
-			end)
-
-			-- buffer diagnostics only
-			vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist)
-
-			vim.keymap.set("n", "[c", function()
-				vim.diagnostic.goto_prev({ wrap = false })
-			end)
-
-			vim.keymap.set("n", "]c", function()
-				vim.diagnostic.goto_next({ wrap = false })
-			end)
-
-			-- Example mappings for usage with nvim-dap. If you don't use that, you can
-			-- skip these
-			vim.keymap.set("n", "<leader>dc", function()
-				require("dap").continue()
-			end)
-
-			vim.keymap.set("n", "<leader>dr", function()
-				require("dap").repl.toggle()
-			end)
-
-			vim.keymap.set("n", "<leader>dK", function()
-				require("dap.ui.widgets").hover()
-			end)
-
-			vim.keymap.set("n", "<leader>dt", function()
-				require("dap").toggle_breakpoint()
-			end)
-
-			vim.keymap.set("n", "<leader>dso", function()
-				require("dap").step_over()
-			end)
-
-			vim.keymap.set("n", "<leader>dsi", function()
-				require("dap").step_into()
-			end)
-
-			vim.keymap.set("n", "<leader>dl", function()
-				require("dap").run_last()
-			end)
 		end
 
 		return metals_config
