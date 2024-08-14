aa348b1cbd09b57e148920b1184b11daffa0ac46
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-08-14 12:25:16 -0700

    append deprecated to replaced with add

--- vimconfig/nvim/lua/miraie/plugins/harpoon.lua
+++ vimconfig/nvim/lua/miraie/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
 		harpoon.setup()
 
 		vim.keymap.set("n", "<leader>ha", function()
-			harpoon:list():append()
+			harpoon:list():add()
 		end)
 		-- This will show only plain files in the harpoon and not preview
 		-- vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
