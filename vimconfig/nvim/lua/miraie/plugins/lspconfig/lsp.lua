033e0f2c97aa5e5ef4094a4eba6b67041b78f72f
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-08-14 23:17:06 -0700

    Formatting and additional dependencies for fuzzy searching

--- vimconfig/nvim/lua/miraie/plugins/lspconfig/lsp.lua
+++ vimconfig/nvim/lua/miraie/plugins/lspconfig/lsp.lua
@@ -1,173 +1,204 @@
 return {
 
-	{ -- LSP Configuration & Plugins
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
-		dependencies = {
-			-- Automatically install LSPs and related tools to stdpath for neovim
-			"neovim/nvim-lspconfig",
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- LSP Configuration & Plugins
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	dependencies = {
+		-- Automatically install LSPs and related tools to stdpath for neovim
+		"neovim/nvim-lspconfig",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-			-- Useful status updates for LSP.
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", opts = {} },
+		-- Useful status updates for LSP.
+		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+		{ "j-hui/fidget.nvim", opts = {} },
 
-			-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-			-- used for completion, annotations and signatures of Neovim apis
-			{ "folke/neodev.nvim", opts = {} },
-		},
-		config = function()
-			-- Brief Aside: **What is LSP?**
-			--
-			-- LSP is an acronym you've probably heard, but might not understand what it is.
-			--
-			-- LSP stands for Language Server Protocol. It's a protocol that helps editors
-			-- and language tooling communicate in a standardized fashion.
-			--
-			-- In general, you have a "server" which is some tool built to understand a particular
-			-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc). These Language Servers
-			-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-			-- processes that communicate with some "client" - in this case, Neovim!
-			--
-			-- LSP provides Neovim with features like:
-			--  - Go to definition
-			--  - Find references
-			--  - Autocompletion
-			--  - Symbol Search
-			--  - and more!
-			--
-			-- Thus, Language Servers are external tools that must be installed separately from
-			-- Neovim. This is where `mason` and related plugins come into play.
-			--
-			-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-			-- and elegantly composed help section, `:help lsp-vs-treesitter`
+		-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		{ "folke/neodev.nvim", opts = {} },
 
-			-- LSP servers and clients are able to communicate to each other what features they support.
-			--  By default, Neovim doesn't support everything that is in the LSP Specification.
-			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "L3MON4D3/LuaSnip" }, -- Required
+		{ "rafamadriz/friendly-snippets" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "saadparwaiz1/cmp_luasnip" },
+	},
+	config = function()
+		-- Brief Aside: **What is LSP?**
+		--
+		-- LSP is an acronym you've probably heard, but might not understand what it is.
+		--
+		-- LSP stands for Language Server Protocol. It's a protocol that helps editors
+		-- and language tooling communicate in a standardized fashion.
+		--
+		-- In general, you have a "server" which is some tool built to understand a particular
+		-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc). These Language Servers
+		-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
+		-- processes that communicate with some "client" - in this case, Neovim!
+		--
+		-- LSP provides Neovim with features like:
+		--  - Go to definition
+		--  - Find references
+		--  - Autocompletion
+		--  - Symbol Search
+		--  - and more!
+		--
+		-- Thus, Language Servers are external tools that must be installed separately from
+		-- Neovim. This is where `mason` and related plugins come into play.
+		--
+		-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
+		-- and elegantly composed help section, `:help lsp-vs-treesitter`
 
-			-- Enable the following language servers
-			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-			--
-			--  Add any additional override configuration in the following tables. Available keys are:
-			--  - cmd (table): Override the default command used to start the server
-			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
-			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-			local lspconfig = require("lspconfig")
-			local util = require("lspconfig/util")
-			local servers = {
-				-- clangd = {},
-				gopls = {
-					-- Require LSP config which we can use to attach gopls
-					-- Since we installed lspconfig and imported it, we can reach
-					-- gopls by lspconfig.gopls
-					-- we can then set it up using the setup and insert the needed configurations
-					lspconfig.gopls.setup({
-						capabilities = capabilities,
-						cmd = { "gopls", "serve" },
-						filetypes = { "go", "gomod" },
-						root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-						settings = {
-							gopls = {
-								analyses = {
-									unusedparams = true,
-								},
-								staticcheck = true,
-							},
-						},
-					}),
-				},
+		-- LSP servers and clients are able to communicate to each other what features they support.
+		--  By default, Neovim doesn't support everything that is in the LSP Specification.
+		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-				--csharp_ls = {
-				--	lspconfig.csharp_ls.setup({
-				--		capabilities = capabilities,
-				--		handlers = {
-				--			--			["textDocument/definition"] = require("csharpls_extended").handler,
-				--			--			["textDocument/typeDefinition"] = require("csharpls_extended").handler,
-				--		},
-				--		cmd = { "csharpls" },
-				--		filetypes = { "cs" },
-				--		root_dir = function(fname)
-				--			return util.root_pattern("*.sln")(fname) or util.root_pattern("*.csproj")(fname)
-				--		end,
-				--		init_options = {
-				--			AutomaticWorkspaceInit = true,
-				--		},
-				--	}),
-				--},
-
-				pyright = {
-					lspconfig.pyright.setup({
-						capabilities = capabilities,
-						settings = {
-							pyright = {
-								analysis = {
-									useLibraryCodeForTypes = true,
-								},
-							},
-						},
-					}),
-				},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
-				--
-
-				lua_ls = {
-					filetypes = { "lua" },
+		-- Enable the following language servers
+		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+		--
+		--  Add any additional override configuration in the following tables. Available keys are:
+		--  - cmd (table): Override the default command used to start the server
+		--  - filetypes (table): Override the default list of associated filetypes for the server
+		--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+		--  - settings (table): Override the default settings passed when initializing the server.
+		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+		local lspconfig = require("lspconfig")
+		local util = require("lspconfig/util")
+		local cmp = require("cmp")
+		local servers = {
+			-- clangd = {},
+			gopls = {
+				-- Require LSP config which we can use to attach gopls
+				-- Since we installed lspconfig and imported it, we can reach
+				-- gopls by lspconfig.gopls
+				-- we can then set it up using the setup and insert the needed configurations
+				lspconfig.gopls.setup({
 					capabilities = capabilities,
+					cmd = { "gopls", "serve" },
+					filetypes = { "go", "gomod" },
+					root_dir = util.root_pattern("go.work", "go.mod", ".git"),
 					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
+						gopls = {
+							analyses = {
+								unusedparams = true,
 							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
+							staticcheck = true,
 						},
 					},
-				},
-			}
+				}),
+			},
 
-			-- Ensure the servers and tools above are installed
-			--  To check the current status of installed tools and/or manually install
-			--  other tools, you can run
-			--    :Mason
+			--csharp_ls = {
+			--	lspconfig.csharp_ls.setup({
+			--		capabilities = capabilities,
+			--		handlers = {
+			--			--			["textDocument/definition"] = require("csharpls_extended").handler,
+			--			--			["textDocument/typeDefinition"] = require("csharpls_extended").handler,
+			--		},
+			--		cmd = { "csharpls" },
+			--		filetypes = { "cs" },
+			--		root_dir = function(fname)
+			--			return util.root_pattern("*.sln")(fname) or util.root_pattern("*.csproj")(fname)
+			--		end,
+			--		init_options = {
+			--			AutomaticWorkspaceInit = true,
+			--		},
+			--	}),
+			--},
+
+			pyright = {
+				lspconfig.pyright.setup({
+					capabilities = capabilities,
+					settings = {
+						pyright = {
+							analysis = {
+								useLibraryCodeForTypes = true,
+							},
+						},
+					},
+				}),
+			},
+			-- rust_analyzer = {},
+			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
-			--  You can press `g?` for help in this menu
-			require("mason").setup()
+			-- Some languages (like typescript) have entire language plugins that can be useful:
+			--    https://github.com/pmizio/typescript-tools.nvim
+			--
+			-- But for many setups, the LSP (`tsserver`) will work just fine
+			-- tsserver = {},
+			--
 
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format lua code
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
+			lua_ls = {
+				filetypes = { "lua" },
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
+						},
+						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+						-- diagnostics = { disable = { 'missing-fields' } },
+					},
 				},
-			})
-		end,
-	},
+			},
+		}
+		-- `/` cmdline setup.
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		-- `:` cmdline setup.
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+		-- Ensure the servers and tools above are installed
+		--  To check the current status of installed tools and/or manually install
+		--  other tools, you can run
+		--    :Mason
+		--
+		--  You can press `g?` for help in this menu
+		require("mason").setup()
+
+		-- You can add other tools here that you want Mason to install
+		-- for you, so that they are available from within Neovim.
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua", -- Used to format lua code
+		})
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					-- This handles overriding only values explicitly passed
+					-- by the server configuration above. Useful when disabling
+					-- certain features of an LSP (for example, turning off formatting for tsserver)
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
+	end,
 }
