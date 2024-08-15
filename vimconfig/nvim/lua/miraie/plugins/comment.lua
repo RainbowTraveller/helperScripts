43ca70ed305c4f6f7135d5dfdaa263d09e665dc4
RainbowTraveller <milindarvindvaidya@gmail.com>
2024-08-14 23:10:31 -0700

    Customized the key binding for comment plugin

--- vimconfig/nvim/lua/miraie/plugins/comment.lua
+++ vimconfig/nvim/lua/miraie/plugins/comment.lua
@@ -1,6 +1,31 @@
 return {
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+
+			-- LHS of toggle mappings in NORMAL mode
+			toggler = {
+				--Line-comment toggle keymap
+				line = "lcc",
+				--Block-comment toggle keymap
+				block = "lbc",
+			},
+			--LHS of operator-pending mappings in NORMAL and VISUAL mode
+			opleader = {
+				--Line-comment keymap
+				line = "lc",
+				--Block-comment keymap
+				block = "lb",
+			},
+			--LHS of extra mappings
+			extra = {
+				--Add comment on the line above
+				above = "lcO",
+				--Add comment on the line below
+				below = "lco",
+				--Add comment at the end of line
+				eol = "lcA",
+			},
+		},
+	},
 }
-
-
