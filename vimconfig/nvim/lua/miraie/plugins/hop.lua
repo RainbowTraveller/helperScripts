60ff7d21e46eb292126c93590323080e7ce7370f
RainbowTraveller <milindarvindvaidya@gmail.com>
2025-03-17 12:07:35 -0700

    Adding word hop and changing the keys for Hop

--- vimconfig/nvim/lua/miraie/plugins/hop.lua
+++ vimconfig/nvim/lua/miraie/plugins/hop.lua
@@ -7,19 +7,23 @@ return {
 		local hop = require("hop")
 		local directions = require("hop.hint").HintDirection
 
-		vim.keymap.set("", "<leader>c", function()
+		vim.keymap.set("", "<leader>oc", function()
 			hop.hint_char1({})
 		end, { remap = true })
 
-		vim.keymap.set("", "<leader>l", function()
+		vim.keymap.set("", "<leader>ol", function()
 			hop.hint_lines_skip_whitespace({})
 		end, { remap = true })
 
-		vim.keymap.set("", "<leader>v", function()
+		vim.keymap.set("", "<leader>ov", function()
 			hop.hint_vertical({})
 		end, { remap = true })
 
-		vim.keymap.set("", "<leader>a", function()
+		vim.keymap.set("", "<leader>oa", function()
+			hop.hint_anywhere({})
+		end, { remap = true })
+
+		vim.keymap.set("", "<leader>ow", function()
 			hop.hint_anywhere({})
 		end, { remap = true })
 	end,
